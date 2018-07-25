43a1e04387d06b5aef5e9b132fb3c6cd7c425a2e
Joris de Keijser <chinook25@gmail.com>
2018-07-25 10:55:44 +0200

    first version

---
+++ pageTitleService.js
@@ -0,0 +1,29 @@
+'use strict';
+define(['angular'], function(angular) {
+  angular.module('pageTitleService', [])
+    .factory('PageTitleService', ['$state', function($state) {
+      var validStateControllerCombinations = {};
+
+      function loadLexicon(whiteListPromise) {
+        whiteListPromise.then(function(result) {
+          var loadedWhiteList = result.data;
+          for (var stateName in loadedWhiteList){
+            if(loadedWhiteList.hasOwnProperty(stateName)){
+              validStateControllerCombinations[stateName] = loadedWhiteList[stateName];
+            }
+          }
+        });
+      }
+
+      function setPageTitle(controllerName, newTitle) {
+        if (validStateControllerCombinations[$state.current.name] === controllerName) {
+          document.title = newTitle;
+        }
+      }
+      
+      return {
+        loadLexicon: loadLexicon,
+        setPageTitle: setPageTitle
+      };
+    }]);
+});
