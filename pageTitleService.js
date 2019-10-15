a6f425958f10f39bbe99ee3996691786a01d2300
karlis <karlis.zalite@gmail.com>
2019-10-15 14:01:35 +0200

    fix race condition; version bump

--- pageTitleService.js
+++ pageTitleService.js
@@ -3,22 +3,24 @@ define(['angular'], function(angular) {
   angular.module('page-title-service', [])
     .factory('PageTitleService', ['$state', function($state) {
       var validStateControllerCombinations = {};
+      var combinationsPromise;
 
       function loadLexicon(newCombinationsPromise) {
-        newCombinationsPromise.then(function(result) {
-          var newCombinations = result.data || result;
-          for (var stateName in newCombinations){
-            if(newCombinations.hasOwnProperty(stateName)){
-              validStateControllerCombinations[stateName] = newCombinations[stateName];
-            }
-          }
-        });
+        combinationsPromise = newCombinationsPromise;
       }
 
       function setPageTitle(controllerName, newTitle) {
-        if (validStateControllerCombinations[$state.current.name] === controllerName) {
-          document.title = newTitle;
-        }
+        combinationsPromise.then(function(result) {
+          var newCombinations = result.data || result;
+          for (var stateName in newCombinations) {
+            if (newCombinations.hasOwnProperty(stateName)) {
+              validStateControllerCombinations[stateName] = newCombinations[stateName];
+            }
+          }
+          if (validStateControllerCombinations[$state.current.name] === controllerName) {
+            document.title = newTitle;
+          }
+        });
       }
 
       return {
